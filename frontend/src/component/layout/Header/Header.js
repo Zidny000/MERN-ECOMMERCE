76febbd72e73fdcb85ee05f75facd36fd1653c10
zidny000 <t.m.zidny@gmail.com>
2022-07-27 23:58:52 +0600

    Menu link correction

--- frontend/src/component/layout/Header/Header.js
+++ frontend/src/component/layout/Header/Header.js
@@ -1,22 +1,33 @@
-import React from "react";
+import {useEffect,useRef} from "react";
+import {Link,useLocation} from "react-router-dom"
+import { useSelector } from "react-redux";
 
 const Header = () =>{
+    const location = useLocation()
+    const inputCheck = useRef(null);
+    const { user, loading, isAuthenticated } = useSelector((state) => state.user);
+    useEffect(
+      () => {
+        inputCheck.current.checked = false
+      },
+      [location]
+    )
     return(
       <div className="navigation">
-        <input type="checkbox" className="navigation__checkbox" id="navi-toggle" />
+          <input type="checkbox" ref={inputCheck} className="navigation__checkbox" id="navi-toggle" />
         <label for="navi-toggle" className="navigation__button">
           <span className="navigation__icon">
             &nbsp;
           </span>
         </label>
         
-        <div className="navigation__background">&nbsp;</div>
+        <div className="navigation__background">&nbsp;</div> 
           <nav className="navigation__nav">
             <ul className="navigation__list">					
-              <li className="navigation__item"><a href="/" className="navigation__link">Home</a></li>
-              <li className="navigation__item"><a href="/" className="navigation__link">Shop</a></li>
-              <li className="navigation__item"><a href="/" className="navigation__link">Login</a></li>
-              <li className="navigation__item"><a href="/" className="navigation__link">Register</a></li>
+              <li className="navigation__item"><Link to="/" className="navigation__link">Home</Link></li>
+              <li className="navigation__item"><Link to="/products" className="navigation__link">Shop</Link></li>
+              <li className="navigation__item"><Link to="/search" className="navigation__link">Search</Link></li>
+              {!isAuthenticated && <li className="navigation__item"><Link to="/login" className="navigation__link">Login</Link></li>}
             </ul>
           </nav>
         
